d567b94106e344549504dae295e52d54e34aade8
李藜 <lili2018@xinhua.org>
2021-08-20 15:51:49 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { h1 } from './m1'
+import { h1, person } from './m1'
 
 function sum(a: number, b: number): number {
     return a + b;
 }
-debugger
 console.log(sum(2, 3))
 console.log(h1);
+console.log(person.name);
--- src/m1.ts
+++ src/m1.ts
@@ -1,1 +1,7 @@
 export const h1 = '你好'
+
+class Person {
+    name: string = '孙悟空';
+    age: number = 18;
+}
+export const person: Person = new Person();
